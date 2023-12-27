7bcf418eee8804af5fe3eac616fe5ebe84915fec
guzhifu <guzf@zhimadi.cn>
2023-12-27 15:45:00 +0800

    自定义\App\Listener\ErrorExceptionHandler类记录错误日志

--- app/Listener/ErrorExceptionHandler.php
+++ app/Listener/ErrorExceptionHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 /**
  * 错误日志记录
  */
+
 namespace App\Listener;
 
 use Hyperf\Contract\StdoutLoggerInterface;
@@ -24,10 +25,10 @@ class ErrorExceptionHandler implements ListenerInterface
     {
         //错误记录日志，dev环境下直接输出到命令行，线上环境记录到runtime/logs目录下
         set_error_handler(static function ($level, $message, $file = '', $line = 0): bool {
-            $outputLogger = env("APP_ENV",'dev') == 'dev'
+            $outputLogger = env("APP_ENV", 'dev') == 'dev'
                 ? make(StdoutLoggerInterface::class)
-                : make(LoggerFactory::class)->get('log','default');
-            $logFormat = sprintf("[level]:%s---[file]:%s---[line]:%s---[message]:%s",$level,$file,$line,$message);
+                : make(LoggerFactory::class)->get('log', 'default');
+            $logFormat = sprintf("[level]:%s---[file]:%s---[line]:%s---[message]:%s", $level, $file, $line, $message);
             $outputLogger->error($logFormat);
             return true;
         });
